fd83eea2376c4737f32bde8f15b51e704ecabd97
ArthurSampaio13 <arthur.sampaio@upe.br>
2025-05-02 15:51:44 -0300

    feat: __init__

---
+++ redacao_detector/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.0"
+
+from redacao_detector.detector import (
+    corrigir_rotacao,
+    detectar_areas_texto,
+    processar_imagem,
+    processar_diretorio,
+)
+
+__all__ = ["corrigir_rotacao", "detectar_areas_texto", "processar_imagem", "processar_diretorio"]
